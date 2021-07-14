a20406066d59e80bb07fc2e8a7dd00d216218076
Eng-Abanoub <>
2021-07-14 02:12:41 +0200

    add skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,26 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.querySelectorAll('.skills__content')
+const skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+
+    if(this.parentElement.classList.contains('skills__opened')){
+        this.parentElement.classList.remove('skills__opened')
+        this.parentElement.classList.add('skills__closed')
+    }
+    else{
+    for(i=0;i<skillsContent.length;i++){
+        if(skillsContent[i].classList.contains('skills__opened')){
+            skillsContent[i].classList.remove('skills__opened')
+            skillsContent[i].classList.add('skills__closed')
+        }
+    }
+    this.parentElement.classList.add('skills__opened')   
+}}
+
+skillsHeader.forEach((el)=>el.addEventListener('click',toggleSkills))
 
 /*==================== QUALIFICATION TABS ====================*/
 
